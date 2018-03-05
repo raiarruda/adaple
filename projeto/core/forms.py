a11ef12e20567a20d5d2deb58091fca67373d229
raissa <raiarruda.raa@gmauil.com>
2018-03-05 15:19:46 -0300

    erro utf-8

--- projeto/core/forms.py
+++ projeto/core/forms.py
@@ -20,43 +20,15 @@ class BootstrapAuthenticationForm(AuthenticationForm):
 class form_edp(forms.ModelForm):
     nome = forms.CharField(label='Nome')
     objetivo_pedagogigo = forms.CharField(label= 'O que aprender? ', widget=forms.Textarea)
-   # habilidades = forms.ModelChoiceField(queryset=db_habilidades.objects.all(), label= 'Habilidades Trabalhadas',required=True , widget=forms.CheckboxSelectMultiple)    
-    habilidades =  forms.MultipleChoiceField(label = 'Quais habilidades envolvidas?', required=True, widget=forms.CheckboxSelectMultiple, choices=skills_choice,    )
+    habilidades = forms.ModelChoiceField(queryset=db_habilidades.objects.all(), label= 'Habilidades Trabalhadas',required=True , widget=forms.CheckboxSelectMultiple)    
+   # habilidades =  forms.MultipleChoiceField(label = 'Quais habilidades envolvidas?', required=True, widget=forms.CheckboxSelectMultiple, choices=skills_choice,    )
     metodologia = forms.CharField(label= 'O que fazer? ', widget=forms.Textarea)
-  #  player_video = forms.BooleanField(label= 'Usar visualizador de vídeo?')
-   # video= forms.URLField(label= 'Qual o endereço (URL) do vídeo?')
-    # anotacao_texto
-    # player_audio
-    # player_imagem
-    # carregar_video
-    # carregar_audio
-    # carregar_imagem 
+
     class Meta:
          model = db_edp
        
-         fields = ('nome', 'video')
-    #     'objetivo_pedagogigo',
-    #     'habilidades' ,
-    #     'metodologia', 
-    #     'player_video',
-    #     'video',
-    #     'anotacao_texto',
-    #     'player_audio' ,
-    #     'player_imagem',
-    #     'carregar_video',
-    #     'carregar_audio' ,
- #        'carregar_imagem' ,)
+         fields = ('nome', )
 
-
-#class EscritaForm(forms.ModelForm):
-
-#    paragrafo = forms.CharField(widget=forms.Textarea(  {
-#        'class': 'form-control',
-#        'placeholder': 'digite o paragrafo'}  ))
-
- #   class Meta:
-  #      model = db_escrita
-   #     fields = ('paragrafo','posicao_paragrafo','sala')
 
 class form_edp_aluno(forms.ModelForm):
      class Meta:
--- projeto/core/models.py
+++ projeto/core/models.py
@@ -15,28 +15,27 @@ class db_edp(models.Model):
     nome = models.CharField(max_length=100)
     objetivo_pedagogigo = models.TextField()
     habilidades = models.ManyToManyField(db_habilidades)
-  
     metodologia = models.TextField()
     usuario = models.ForeignKey(User,on_delete=models.DO_NOTHING,)
 
 
-    # escolha do professor
-    player_video = models.NullBooleanField(blank=True, null=True)
-    #isso só deve ter caso player_video seja True
-    video = EmbedVideoField(blank=True, null=True)
+    # # escolha do professor
+    # player_video = models.NullBooleanField(blank=True, null=True)
+    # #isso só deve ter caso player_video seja True
+    # video = EmbedVideoField(blank=True, null=True)
     
-    anotacao_texto = models.NullBooleanField(blank=True, null=True)
-    texto = models.TextField(blank=True, null=True)
+    # anotacao_texto = models.NullBooleanField(blank=True, null=True)
+    # texto = models.TextField(blank=True, null=True)
     
-    player_audio = models.NullBooleanField(blank=True, null=True)
+    # player_audio = models.NullBooleanField(blank=True, null=True)
     
-    player_imagem = models.NullBooleanField(blank=True, null=True)
+    # player_imagem = models.NullBooleanField(blank=True, null=True)
     
-    carregar_video = models.NullBooleanField(blank=True, null=True)
+    # carregar_video = models.NullBooleanField(blank=True, null=True)
     
-    carregar_audio = models.NullBooleanField(blank=True, null=True)
+    # carregar_audio = models.NullBooleanField(blank=True, null=True)
     
-    carregar_imagem = models.NullBooleanField(blank=True, null=True)
+    # carregar_imagem = models.NullBooleanField(blank=True, null=True)
 
     def iniciar(self):
         self.save()
@@ -44,6 +43,32 @@ class db_edp(models.Model):
     def __str__ (self):
         return self.nome
     
+class recursos (models.Model):
+    edp = models.ForeignKey(db_edp, on_delete=models.DO_NOTHING)
+
+    #escolha do professor
+    # player_video = models.NullBooleanField(blank=True, null=True)
+    # #isso só deve ter caso player_video seja True
+    video = EmbedVideoField(blank=True, null=True)
+    
+    # anotacao_texto = models.NullBooleanField(blank=True, null=True)
+    texto = models.TextField(blank=True, null=True)
+    
+    player_audio = models.NullBooleanField(blank=True, null=True)
+    
+    player_imagem = models.NullBooleanField(blank=True, null=True)
+    
+    # carregar_video = models.NullBooleanField(blank=True, null=True)
+    
+    # carregar_audio = models.NullBooleanField(blank=True, null=True)
+    
+    # carregar_imagem = models.NullBooleanField(blank=True, null=True)
+
+    def iniciar(self):
+        self.save()
+
+    def __str__ (self):
+        return self.edp.nome
 class db_edp_aluno(models.Model):
   
     aluno = models.ForeignKey(User, on_delete=models.DO_NOTHING,)
